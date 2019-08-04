ee7a08fdad7fe5e1f3f1770686955dd60b05376e
Wenz11 <angzw-wm17@student.tarc.edu.my>
2019-08-04 20:20:43 +0800

    Added New Material Design Support

--- app/src/main/java/com/example/help4u/MainActivity.java
+++ app/src/main/java/com/example/help4u/MainActivity.java
@@ -187,8 +187,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
