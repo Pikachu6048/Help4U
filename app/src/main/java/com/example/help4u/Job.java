464b23fc42debdc26ff844856f8fb55bd705ef7e
yeuharn <limyh-wm17@student.tarc.edu.my>
2019-08-16 22:17:43 +0800

    Added Qualification and modify JobList layout

---
+++ app/src/main/java/com/example/help4u/Job.java
@@ -0,0 +1,18 @@
+package com.example.help4u;
+
+/*
+* Created by YeuHarn
+* An abstract class that holds constants related to jobs
+* */
+public abstract class Job {
+    public static final int TOTAL_NUM_OF_JOB_CATEGORY = 2; //total number of job category
+    public static final int TOTAL_NUM_OF_JOB = 4; //total number of jobs
+
+    public static final String IT_CATEGORY_DESC = "IT"; //IT category description
+    public static final String BUSINESS_CATEGORY_DESC = "Business"; //Business category description
+
+    public static final int PROGRAMMER = 0; //constant index number for Programmer
+    public static final int DATA_SCIENTIST = 1; //constant index number for DataScientist
+    public static final int ACCOUNTANT = 2; //constant index number for Accountant
+    public static final int ENTREPRENEUR = 3; //constant index number for Entrepreneur
+}
---
+++ app/src/main/java/com/example/help4u/Qualification.java
@@ -0,0 +1,47 @@
+package com.example.help4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/*
+* Created by YeuHarn
+* An activity for user to choose the qualification before taking career test
+* */
+public class Qualification extends AppCompatActivity {
+
+    public static final String QUALIFICATION = "com.example.help4u.Qualification"; //to transfer user qualification to following activities
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_qualification );
+
+        //set "IT" as user qualification
+        Button qualiIT = (Button) findViewById( R.id.button_quali_IT );
+        qualiIT.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCareerQuestionnaire( Job.IT_CATEGORY_DESC );
+            }
+        } );
+
+        //set "Business" as user qualification
+        Button qualiBusiness = (Button) findViewById( R.id.button_quali_business );
+        qualiBusiness.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCareerQuestionnaire( Job.BUSINESS_CATEGORY_DESC );
+            }
+        } );
+    }
+
+    private void launchCareerQuestionnaire(String qualification){
+        Intent intent = new Intent( this, CareerQuestionnaire.class );
+        intent.putExtra( QUALIFICATION, qualification ); //transfer the selected qualification to following activity
+        startActivity( intent );
+    }
+}
