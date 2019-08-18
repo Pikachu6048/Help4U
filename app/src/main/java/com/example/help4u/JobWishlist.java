96967aab50e80af2dc7e2be85f2c02e434272ebf
Wenz11 <angzw-wm17@student.tarc.edu.my>
2019-08-18 14:59:09 +0800

    Added Writing To Wish List

--- app/src/main/java/com/example/help4u/JobWishlist.java
+++ app/src/main/java/com/example/help4u/JobWishlist.java
@@ -1,14 +1,97 @@
 package com.example.help4u;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class JobWishlist extends AppCompatActivity {
+
+    // Get Database Reference
+    DatabaseReference reference;
+
+    // Declare Recylcer View
+    RecyclerView recyclerView;
+
+    // Array List of Job Wishlist
+    ArrayList<WishList> jobwishlist;
+
+    // Set Adapter
+    MyJobWishlist adapter;
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_wishlist);
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        recyclerView = findViewById(R.id.MyJobWishListRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        jobwishlist = new ArrayList<WishList>();
+
+        reference = FirebaseDatabase.getInstance().getReference().child("jobwishlist").child(mFirebaseUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+                {
+                    WishList wl = dataSnapshot1.getValue(WishList.class);
+                    jobwishlist.add(wl);
+                }
+                adapter = new MyJobWishlist(JobWishlist.this, jobwishlist);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Show Error Message
+            }
+        });
+
+        // Add the functionality to swipe items in the
+        // recycler view to delete that item
+        ItemTouchHelper helper = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(0,
+                        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                    @Override
+                    public boolean onMove(RecyclerView recyclerView,
+                                          RecyclerView.ViewHolder viewHolder,
+                                          RecyclerView.ViewHolder target) {
+                        return false;
+                    }
+
+                    @Override
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder,
+                                         int direction) {
+//                    int position = viewHolder.getAdapterPosition();
+
+                        // Delete the word
+                        Toast.makeText(JobWishlist.this,"Deleting...",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        helper.attachToRecyclerView(recyclerView);
     }
 }
---
+++ app/src/main/java/com/example/help4u/MyJobWishlist.java
@@ -0,0 +1,70 @@
+package com.example.help4u;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class MyJobWishlist extends RecyclerView.Adapter<MyJobWishlist.MyAllJobWishLists> {
+
+    Context context;
+    ArrayList<WishList> jobwishlist;
+
+    // To delete item from database
+    ArrayList<WishList> jwl;
+    ArrayList<String> keys;
+
+    public MyJobWishlist(Context c, ArrayList<WishList> wl)
+    {
+        context = c;
+        jobwishlist = wl;
+    }
+
+    @NonNull
+    @Override
+    public MyAllJobWishLists onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyAllJobWishLists(LayoutInflater.from(context).inflate(R.layout.jobwishlistcardview, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAllJobWishLists holder, int position) {
+        holder.mgetjobtitle.setText(jobwishlist.get(position).getJobtitle());
+        holder.mgetjobdescription.setText(jobwishlist.get(position).getJobdescription());
+        holder.mgetposition.setText(jobwishlist.get(position).getPosition());
+        Picasso.get().load(jobwishlist.get(position).getPhotourl()).into(holder.mgetphotourl);;
+    }
+
+    @Override
+    public int getItemCount() {
+        return jobwishlist.size();
+    }
+
+    class MyAllJobWishLists extends RecyclerView.ViewHolder
+    {
+        // Declare Variables in job wish list
+        ImageView mgetphotourl;
+        TextView mgetjobtitle, mgetjobdescription, mgetposition;
+
+        public MyAllJobWishLists(@NonNull View itemView) {
+            super(itemView);
+            mgetphotourl = itemView.findViewById(R.id.getphotourl);
+            mgetjobtitle = itemView.findViewById(R.id.getjobtitle);
+            mgetjobdescription = itemView.findViewById(R.id.getjobdescription);
+            mgetposition = itemView.findViewById(R.id.getposition);
+        }
+    }
+}
